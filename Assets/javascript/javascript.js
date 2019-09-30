14cda19f2ce09e275cfb030fcf3dd58c9d327835
Joshua Einsel <Joshua.einsel@live.com>
2019-09-30 14:24:33 -0700

    Added folders and index.html

---
+++ Assets/javascript/javascript.js
@@ -0,0 +1,18 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyC1YJufOXeRj4Fmf71hu1zvDCLvotZ_uB8",
+    authDomain: "rps-multiplayer-40d11.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-40d11.firebaseio.com",
+    projectId: "rps-multiplayer-40d11",
+    storageBucket: "",
+    messagingSenderId: "808909169259",
+    appId: "1:808909169259:web:63f07c749eb29288df872e"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database()
+
+database.set(){
+    var test = true
+}
